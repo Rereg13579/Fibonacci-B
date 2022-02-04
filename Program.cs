b592fed00339e7ff22b6df182af284a5c97afb94
reg bl <untergrund1992@hotmail.com>
2022-02-04 18:40:19 +0100

    added cli args

--- Program.cs
+++ Program.cs
@@ -1,28 +1,39 @@
 ﻿using System;
 
-namespace Fibonacci {
-    static class Program {
-        static void Main(string[] args) {
-            int[] fibonacci = CreateFibonacciSeries(5);
+namespace Fibonacci
+{
+  static class Program
+  {
+    static void Main(string[] args)
+    {
+      int num = 5;
+      if(args.Length == 1)
+      {
+        num = int.Parse(args[0]);
+      }
+      int[] fibonacci = CreateFibonacciSeries(num);
 
-            for (int i = 0; i < fibonacci.Length; i++) {
-                Console.Out.Write(fibonacci[i]);
-                Console.Out.Write(" ");
-            }
-            Console.Out.WriteLine();
-        }
-
-        static int CreateFibonacciNumber(int index) {
-            if (index == 0) return 0;
-            if (index == 1) return 1;
-            return CreateFibonacciNumber(index - 1) + CreateFibonacciNumber(index - 2);
-        }
-
-        static int[] CreateFibonacciSeries(int count) {
-            int[] fibonacciSeries = new int[count];
-            for (int i = 0; i < count; i++)
-                fibonacciSeries[i] = CreateFibonacciNumber(i);
-            return fibonacciSeries;
-        }
+      for (int i = 0; i < fibonacci.Length; i++)
+      {
+        Console.Out.Write(fibonacci[i]);
+        Console.Out.Write(" ");
+      }
+      Console.Out.WriteLine();
     }
+
+    static int CreateFibonacciNumber(int index)
+    {
+      if (index == 0) return 0;
+      if (index == 1) return 1;
+      return CreateFibonacciNumber(index - 1) + CreateFibonacciNumber(index - 2);
+    }
+
+    static int[] CreateFibonacciSeries(int count)
+    {
+      int[] fibonacciSeries = new int[count];
+      for (int i = 0; i < count; i++)
+        fibonacciSeries[i] = CreateFibonacciNumber(i);
+      return fibonacciSeries;
+    }
+  }
 }
